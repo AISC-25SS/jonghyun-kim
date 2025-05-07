cbd93113cd285499536bc44f2dcaa97931b67efc
user <poreer0597@hs.ac.kr>
2025-05-07 13:15:10 +0900

    chore: success java project

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import test.GitTest;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("Hello 종현 테스트");
 
         GitTest gitTest = new GitTest();
 
